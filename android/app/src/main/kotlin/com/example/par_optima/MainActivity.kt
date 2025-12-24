6a6b6e9d4878bf3b6d4d4172fd149b44ef47138d
Kanchan <kanchanshrestha030716@gmail.com>
2025-12-24 17:27:01 +0545

    IOT and APP seperated

--- android/app/src/main/kotlin/com/example/par_optima/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.par_optima
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../src/screens/login.dart'; // Import your file
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoginPage(), // This tells the app to start on the Login Page
-  ));
-}
